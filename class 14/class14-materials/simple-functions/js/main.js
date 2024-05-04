7c00b66a3841fa8348feee112ccb0667b5fed7fa
Martin <jo.ju.martin@gmail.com>
2024-05-04 17:36:58 +0200

    added class 16

--- class 14/class14-materials/simple-functions/js/main.js
+++ class 14/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,21 @@
-//create a function that adds two numbers and alerts the sum
+
+
+function addNumbers(num1, num2){
+    const sum = num1 + num2
+    alert(sum)
+    return sum
+}
 
 //create a function that multiplys three numbers and console logs the product
 
+function multiply(num1, num2, num3){
+    const product = num1 * num2 * num3
+    console.log(product)
+    return product
+}
 //create a function that divides two numbers and returns the ???
+
+function divide(num1, num2){
+    const result = num1 / num2
+    return result
+}
--- class 16/class16-materials/bring-it-on/js/main.js
+++ class 16/class16-materials/bring-it-on/js/main.js
@@ -1,34 +1,17 @@
 // *Variables*
 // Create a variable and console log the value
 
-
 // Create a variable, add 10 to it, and alert the value
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-function sub(num1,num2,num3,num4){
-    alert(num1-num2-num3-num4)
-}
 
-sub(1,2,3,4)
 // Create a function that divides one number by another and returns the remainder
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-function bigger(num1, num2){
-    if (num1+num2 > 50){
-        alert("Jumanji")
-    }
-}
 
-bigger(20,26)
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
 
-function complex(num1, num2, num3){
-    const product = num1*num2*num3
-    if ((product % 3) === 0){
-        console.log("Zebra")
-    }
-}
-
-complex(2,3,2)
+//*Loops*
+//Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
