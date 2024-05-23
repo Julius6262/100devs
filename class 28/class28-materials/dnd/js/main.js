dc1b07df0705265f374e2a4f691696a9b877275c
Martin <jo.ju.martin@gmail.com>
2024-05-23 14:27:55 +0200

    dnd exercise solved

--- class 28/class28-materials/dnd/js/main.js
+++ class 28/class28-materials/dnd/js/main.js
@@ -8,6 +8,37 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        console.log(data.name)
+        //create element
+        const spellName = document.createElement('h3')
+        // set the text
+        spellName.innerText = data.name 
+
+        // place it correctly to be shown
+        document.querySelector('#spellName').appendChild(spellName)
+
+       console.log(data.classes[0].name) // class
+
+       //create a new element
+       const classH4 = document.createElement('h4')
+
+        // set the text inside the new element
+        classH4.innerText = data.classes[0].name
+
+        // append the element to the correct section and show it in the dom
+       document.querySelector('#class').appendChild(classH4)
+       
+       console.log(data.subclasses)
+       data.subclasses.forEach((element,i) => {
+        //create a new element
+       const subClassH5 = document.createElement('h5')
+
+       //give the element text
+       subClassH5.innerText = element.name
+
+       //show the newelement in the dom and place it correctly
+       document.querySelector('#subClass').appendChild(subClassH5)
+       });
        
       })
       .catch(err => {
