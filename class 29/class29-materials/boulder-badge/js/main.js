bf2015d4edc8e59aafc8d681452fdf47f85df6b0
Martin <jo.ju.martin@gmail.com>
2024-05-25 09:13:30 +0200

    class 30 added

--- class 29/class29-materials/boulder-badge/js/main.js
+++ class 29/class29-materials/boulder-badge/js/main.js
@@ -1,8 +1,47 @@
 // Variables
-//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
+//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team 
+//that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at 
+//level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 
+//and evolves at level 7. Create three variables to store the number of rare candies each of your 
+//pokemon would need to evolve (rare candies increase your level by one). Finally, create a
+// fourth variable named totalCandies that sums all the rare candies you would need. 
+
+// [currrent level, evolve level]
+const bulbasaur = [5,16]
+const caterpie = [1,7]
+const weedle = [1,7]
+
+const rareCandiesBulbasaur = bulbasaur[0] - bulbasaur[1]
+const rareCandiescaterpie = caterpie[0] - caterpie[1]
+const rareCandiesweedle = weedle[0] - weedle[1]
+
+let totalCandies = Math.abs(rareCandiesBulbasaur + rareCandiescaterpie + rareCandiesweedle)
+console.log(totalCandies)
+
 
 //Conditionals And Functions
-//You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+//You have a charmander in your party. Charmander can only battle if the temperature is above 0 
+//degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another 
+//fuction that tells you wheither or not charmander can battle
+
+function convert(fah){
+    return (fah - 32) * 5/9
+}
+function battle(temp){
+    if (temp > 0){
+        console.log('battle time')
+    } else {
+        console.log('to cold') 
+    }
+}
+
+battle(convert(20))
 
 //Loops
-//You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
+//You have joined an undeground pokemon leauge. In this league, trainers can use any 
+//number of pokemon. Print to the console "Pikachu I choose you" x times where x is the
+// number of pokemon the trainer you are battling has in their party
+
+for (let i = 0; i < x; i++){
+    console.log("Pikachu I choose you")
+}
--- class 29/class29-materials/cascade-badge/js/main.js
+++ class 29/class29-materials/cascade-badge/js/main.js
@@ -1,11 +1,48 @@
 //Arrays
-//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. 
+//Please create a function that reverses your list and prints it to the console. 
 
-//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+const orginalList = ['1','2','3']
 
-//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+function reverseList(oL){
+    revL = []
+    for (let i = oL.length-1; i >= 0; i--){
+        revL.push(oL[i])
+    }
+    return revL
+}
+
+console.log(reverseList(orginalList))
+
+// or originalList.reverse()
+
+//Given two integer arrays a, b, both of length >= 1, create a program that 
+//returns true if the sum of the squares of each element in a is strictly 
+//greater than the sum of the cubes of each element in b.
+
+
+//Return a new array consisting of elements which are multiple of their own index in 
+//input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
-//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+function multiple(arr){
+    return arr.filter((e,i) => e % i === 0 )
+}
+
+console.log(multiple([22, -6, 32, 82, 9, 25]))
+//Given an array of integers as strings and numbers, return the sum of the array values
+// as if all were numbers.Return your answer as a number.
+
+const strNum = [1,'2',3,'4']
+let sum = 0
+for (let i = 0; i < strNum.length; i++){
+    sum += Number(strNum[i])
+}
+
+console.log(sum)
+
+let num = strNum.reduce((prev,cur) => Number(prev) + Number(cur))
+
+console.log(num)
