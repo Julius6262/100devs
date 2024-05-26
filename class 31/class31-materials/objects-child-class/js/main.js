11225b2a198d792fb283ca6968ab7ac90d3eb266
Martin <jo.ju.martin@gmail.com>
2024-05-26 14:33:25 +0200

    changes to tic tac toe, and added class 32

--- class 31/class31-materials/objects-child-class/js/main.js
+++ class 31/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,36 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Car{
+    constructor(doors, gear){
+        this.doors = doors
+        this.gear = gear
+    }
+
+    drive(){
+        console.log(`set in ${this.gear} and push speeder`)
+    }
+
+    turnRadioOn(){
+        console.log('radio is on')
+    }
+}
+
+class BMW extends Car{
+    constructor(doors,gear,horsePower){
+        super(doors,gear)
+        this.horsePower = this.horsePower
+    }
+    honk(){
+        console.log('BOOOOTTT')
+    }
+    radio(){
+        super.turnRadioOn()
+    }
+}
+
+const car = new Car(4,"manuel gear")
+console.log(car)
+
+const bmw = new BMW(6,"automatic gear",500)
+console.log(bmw)
+console.log(bmw.radio())
