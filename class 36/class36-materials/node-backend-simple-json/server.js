985b90b612c138f061451ba71037bbbe2d695ecb
Martin <jo.ju.martin@gmail.com>
2024-05-30 14:21:42 +0200

    added class 36

--- class 36/class36-materials/node-backend-simple-json/server.js
+++ class 36/class36-materials/node-backend-simple-json/server.js
@@ -4,27 +4,30 @@ const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
-function serveFile(filename, res) {
-  fs.readFile(filename, function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-  });
-}
-
-
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
   if (page == '/') {
-    serveFile('index.html', res);
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
   }
   else if (page == '/otherpage') {
-    serveFile('otherpage.html', res);
+    fs.readFile('otherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
   }
   else if (page == '/otherotherpage') {
-    serveFile('otherotherpage.html', res);
+    fs.readFile('otherotherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
   }
   else if (page == '/api') {
     if('student' in params){
@@ -54,7 +57,11 @@ const server = http.createServer((req, res) => {
       res.end();
     });
   }else if (page == '/js/main.js'){
-    serveFile('js/main.js', res);
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
   }else{
     figlet('404!!', function(err, data) {
       if (err) {
